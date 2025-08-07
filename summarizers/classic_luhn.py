e00518d34d9d429adaac11bf0255bdcb6e93e876
Walter Telsnig <walter.telsnig@kelag.at>
2025-08-07 10:28:38 +0200

    Update classic_luhn.py

--- summarizers/classic_luhn.py
+++ summarizers/classic_luhn.py
@@ -1,10 +1,17 @@
 from sumy.parsers.plaintext import PlaintextParser
-from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.luhn import LuhnSummarizer
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+class CustomTokenizer:
+    def to_sentences(self, text):
+        return sent_tokenize(text)
+    
+    def to_words(self, sentence):
+        return word_tokenize(sentence)
 
 def summarize_luhn(text, num_sentences=3):
-    # Use default Sumy English tokenizer, which works with nltk.punkt
-    parser = PlaintextParser.from_string(text, Tokenizer("english"))
+    tokenizer = CustomTokenizer()
+    parser = PlaintextParser.from_string(text, tokenizer)
     summarizer = LuhnSummarizer()
     summary = summarizer(parser.document, num_sentences)
     return " ".join(str(sentence) for sentence in summary)
