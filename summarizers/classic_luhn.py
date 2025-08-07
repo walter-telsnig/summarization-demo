cbd4194aa0eaa4ee1d07e6f479a1967d8fe597aa
Walter Telsnig <walter.telsnig@kelag.at>
2025-08-07 10:21:12 +0200

    Add files via upload

--- summarizers/classic_luhn.py
+++ summarizers/classic_luhn.py
@@ -1,13 +1,15 @@
+
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.summarizers.luhn import LuhnSummarizer
 from nltk.tokenize import sent_tokenize
 
 def summarize_luhn(text, num_sentences=3):
-    # Use sentence tokenizer from nltk directly
-    sentences = sent_tokenize(text)
-    joined_text = " ".join(sentences)
-
-    parser = PlaintextParser.from_string(joined_text, lambda txt: sent_tokenize(txt))
+    # Use NLTK's sent_tokenize directly (requires only punkt)
+    parser = PlaintextParser.from_string(text, TokenizerWrapper())
     summarizer = LuhnSummarizer()
     summary = summarizer(parser.document, num_sentences)
     return " ".join(str(sentence) for sentence in summary)
+
+class TokenizerWrapper:
+    def __call__(self, text):
+        return sent_tokenize(text)
