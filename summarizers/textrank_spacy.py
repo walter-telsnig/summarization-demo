de56e904d0b4b5485f59b83dea1ffb89aa1ebb54
Walter Telsnig <walter.telsnig@kelag.at>
2025-08-07 16:11:09 +0200

    Update textrank_spacy.py

--- summarizers/textrank_spacy.py
+++ summarizers/textrank_spacy.py
@@ -1,20 +1,17 @@
-
 import spacy
 import pytextrank
+import os
+from spacy.util import get_package_path
+from pathlib import Path
 
-# Load spaCy English pipeline and add PyTextRank
 def load_pipeline():
     try:
         nlp = spacy.load("en_core_web_sm")
     except OSError:
         from spacy.cli import download
-        download("en_core_web_sm")
-        nlp = spacy.load("en_core_web_sm")
-    if not any(isinstance(pipe[1], pytextrank.TextRank) for pipe in nlp.pipeline):
+        download("en_core_web_sm", False)
+        model_path = get_package_path("en_core_web_sm")
+        nlp = spacy.load(model_path)
+    if "textrank" not in nlp.pipe_names:
         nlp.add_pipe("textrank")
     return nlp
-
-def summarize_spacy_textrank(text, limit_phrases=10):
-    nlp = load_pipeline()
-    doc = nlp(text)
-    return " ".join([str(s) for s in doc._.textrank.summary(limit_phrases=limit_phrases)])
