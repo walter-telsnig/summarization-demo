e5e78b5263eb4e03fd43ec9218b07ce122069448
Walter Telsnig <walter.telsnig@kelag.at>
2025-08-07 11:18:09 +0200

    Delete summarizers/textrank.py

--- summarizers/textrank.py
+++
@@ -1,8 +0,0 @@
-
-from gensim.summarization import summarize
-
-def summarize_textrank(text, ratio=0.2):
-    try:
-        return summarize(text, ratio=ratio)
-    except ValueError:
-        return "Text too short for TextRank summarization."
