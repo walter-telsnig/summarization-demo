fbc0506524b1496a12fcc5e03d968d7d52b58a9c
Walter Telsnig <walter.telsnig@kelag.at>
2025-08-07 16:24:46 +0200

    Add files via upload

--- summarizers/textrank_spacy.py
+++ summarizers/textrank_spacy.py
@@ -1,19 +1,31 @@
 
 import spacy
 import pytextrank
-from spacy.cli import download
-from spacy.util import is_package, get_package_path
+import os
+import tarfile
+import urllib.request
+from pathlib import Path
+
+MODEL_NAME = "en_core_web_sm"
+MODEL_URL = "https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-3.7.1/en_core_web_sm-3.7.1.tar.gz"
+LOCAL_DIR = Path("spacy_model")
+EXTRACTED_PATH = LOCAL_DIR / MODEL_NAME
+
+def download_and_extract_model():
+    LOCAL_DIR.mkdir(exist_ok=True)
+    tar_path = LOCAL_DIR / f"{MODEL_NAME}.tar.gz"
+
+    if not EXTRACTED_PATH.exists():
+        print("Downloading model...")
+        urllib.request.urlretrieve(MODEL_URL, tar_path)
+        print("Extracting model...")
+        with tarfile.open(tar_path, "r:gz") as tar:
+            tar.extractall(path=LOCAL_DIR)
+        os.remove(tar_path)
 
 def load_pipeline():
-    model = "en_core_web_sm"
-    try:
-        nlp = spacy.load(model)
-    except OSError:
-        if not is_package(model):
-            download(model)  # Downloads into user space (safe on Streamlit Cloud)
-        model_path = get_package_path(model)
-        nlp = spacy.load(model_path)
-
+    download_and_extract_model()
+    nlp = spacy.load(EXTRACTED_PATH)
     if "textrank" not in nlp.pipe_names:
         nlp.add_pipe("textrank")
     return nlp
@@ -21,4 +33,4 @@ def load_pipeline():
 def summarize_spacy_textrank(text, limit_phrases=10):
     nlp = load_pipeline()
     doc = nlp(text)
-    return " ".join([str(s) for s in doc._.textrank.summary(limit_phrases=limit_phrases)])
+    return " ".join(str(s) for s in doc._.textrank.summary(limit_phrases=limit_phrases))
