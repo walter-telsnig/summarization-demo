0fd3a90174f25dd067de929164b2741443889377
Walter Telsnig <walter.telsnig@kelag.at>
2025-08-07 11:23:05 +0200

    Update lexrank.py

--- summarizers/lexrank.py
+++ summarizers/lexrank.py
@@ -2,7 +2,7 @@ from sumy.parsers.plaintext import PlaintextParser
 from sumy.summarizers.lex_rank import LexRankSummarizer
 from nltk.tokenize import sent_tokenize, word_tokenize
 
-class CustomTokenizer:
+class NLTKTokenizer:
     def to_sentences(self, text):
         return sent_tokenize(text)
 
@@ -10,7 +10,8 @@ class CustomTokenizer:
         return word_tokenize(sentence)
 
 def summarize_lexrank(text, num_sentences=3):
-    parser = PlaintextParser.from_string(text, CustomTokenizer())
+    tokenizer = NLTKTokenizer()
+    parser = PlaintextParser.from_string(text, tokenizer)
     summarizer = LexRankSummarizer()
     summary = summarizer(parser.document, num_sentences)
     return " ".join(str(sentence) for sentence in summary)
