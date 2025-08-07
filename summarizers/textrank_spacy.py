4347ef0e43325abe289fea867d7f4b761132f9b5
Walter Telsnig <walter.telsnig@kelag.at>
2025-08-07 16:06:29 +0200

    Update textrank_spacy.py

--- summarizers/textrank_spacy.py
+++ summarizers/textrank_spacy.py
@@ -4,9 +4,13 @@ import pytextrank
 
 # Load spaCy English pipeline and add PyTextRank
 def load_pipeline():
-    nlp = spacy.load("en_core_web_sm")
+    try:
+        nlp = spacy.load("en_core_web_sm")
+    except OSError:
+        from spacy.cli import download
+        download("en_core_web_sm")
+        nlp = spacy.load("en_core_web_sm")
     if not any(isinstance(pipe[1], pytextrank.TextRank) for pipe in nlp.pipeline):
-        tr = pytextrank.TextRank()
         nlp.add_pipe("textrank")
     return nlp
 
